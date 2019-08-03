7a70862f319006287f82b165256142a3e0deee86
Italo Rui <italorui@hotmail.com>
2019-08-03 17:54:15 -0300

    Admin - Produtos

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -18,6 +18,7 @@ class Category extends Model
 			":descategory"=>$this->getdescategory()
         ));
 		$this->setData($results[0]);
+		Category::updateFile();
 	}
 public function get($idcategory){
 	$sql =new Sql();
@@ -31,6 +32,14 @@ public function delete(){
 	$sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory", [
 		':idcategory' =>$this->$idcategory()
 	]);
+	}
+public static function updatefile(){
+	$categories = Category::listAll();
+	$html = [];
+	foreach ($categories as $row){
+		array_push($html, <li><a href='/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+	}
+	file_put_contents($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOE."categories-menu.html", implode('',$html));
 	}
 }
 ?>
