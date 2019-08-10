49adfda84a8d4b51794868f9cce6953322399bec
Italo Rui <italorui@hotmail.com>
2019-08-10 12:05:13 -0300

    WEB CEP

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 session_start();
 require_once("vendor/autoload.php");
 use \Slim\Slim;
-//use \Hcode\Page;
-//use \Hcode\PageAdmin;
-//use \Hcode\Model\User;
-//use \Hcode\Model\Category;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+use \Hcode\Model\Category;
 $app = new Slim();
 $app->config('debug', true);
 require_once("functions.php");
