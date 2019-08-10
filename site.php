b6979c137ca767570de0f3a44fad0a2b8f3c5b4e
Italo Rui <italorui@hotmail.com>
2019-08-10 16:34:47 -0300

    finalizaer pedido - Boleto

--- site.php
+++ site.php
@@ -349,54 +349,53 @@ $app->get("/boleto/:idorder", function($idorder){
 	$order = new Order();
 	$order->get((int)$idorder);
 	// DADOS DO BOLETO PARA O SEU CLIENTE
-	$dias_de_prazo_para_pagamento = 10;
-	$taxa_boleto = 5.00;
-	$data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
-	$valor_cobrado = formatPrice($order->getvltotal()); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
-	$valor_cobrado = str_replace(".", "", $valor_cobrado);
-	$valor_cobrado = str_replace(",", ".",$valor_cobrado);
-	$valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
-	$dadosboleto["nosso_numero"] = $order->getidorder();  // Nosso numero - REGRA: Máximo de 8 caracteres!
-	$dadosboleto["numero_documento"] = $order->getidorder();	// Num do pedido ou nosso numero
-	$dadosboleto["data_vencimento"] = $data_venc; // Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA
-	$dadosboleto["data_documento"] = date("d/m/Y"); // Data de emissão do Boleto
-	$dadosboleto["data_processamento"] = date("d/m/Y"); // Data de processamento do boleto (opcional)
-	$dadosboleto["valor_boleto"] = $valor_boleto; 	// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula
-	// DADOS DO SEU CLIENTE
-	$dadosboleto["sacado"] = $order->getdesperson();
-	$dadosboleto["endereco1"] = $order->getdesaddress() . " " . $order->getdesdistrict();
-	$dadosboleto["endereco2"] = $order->getdescity() . " - " . $order->getdesstate() . " - " . $order->getdescountry() . " -  CEP: " . $order->getdeszipcode();
-	// INFORMACOES PARA O CLIENTE
-	$dadosboleto["demonstrativo1"] = "Pagamento de Compra na Loja Hcode E-commerce";
-	$dadosboleto["demonstrativo2"] = "Taxa bancária - R$ 0,00";
-	$dadosboleto["demonstrativo3"] = "";
-	$dadosboleto["instrucoes1"] = "- Sr. Caixa, cobrar multa de 2% após o vencimento";
-	$dadosboleto["instrucoes2"] = "- Receber até 10 dias após o vencimento";
-	$dadosboleto["instrucoes3"] = "- Em caso de dúvidas entre em contato conosco: suporte@hcode.com.br";
-	$dadosboleto["instrucoes4"] = "&nbsp; Emitido pelo sistema Projeto Loja Hcode E-commerce - www.hcode.com.br";
-	// DADOS OPCIONAIS DE ACORDO COM O BANCO OU CLIENTE
-	$dadosboleto["quantidade"] = "";
-	$dadosboleto["valor_unitario"] = "";
-	$dadosboleto["aceite"] = "";		
+    $dias_de_prazo_para_pagamento = 5;
+    $taxa_boleto = 2.95;
+    $data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006";
+    $valor_cobrado = str_replace($order->getvlttotal()); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+    $valor_cobrado = str_replace(",", ".",$valor_cobrado);
+    $valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
+    $dadosboleto["nosso_numero"] = $order->getidorder();  // Nosso numero sem o DV - REGRA: Máximo de 11 caracteres!
+    $dadosboleto["numero_documento"] =$order->getidorder();	// Num do pedido ou do documento = Nosso numero
+    $dadosboleto["data_vencimento"] = $data_venc; // Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA
+    $dadosboleto["data_documento"] = date("d/m/Y"); // Data de emissão do Boleto
+    $dadosboleto["data_processamento"] = date("d/m/Y"); // Data de processamento do boleto (opcional)
+    $dadosboleto["valor_boleto"] = $valor_boleto; 	// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula
+    // INFORMACOES PARA O CLIENTE
+    $dadosboleto["demonstrativo1"] = "Pagamento de Compra na Loja Hit";
+    $dadosboleto["demonstrativo2"] = "Mensalidade referente a nonon nonooon nononon<br>Taxa bancária - R$ ".number_format($taxa_boleto, 2, ',', '');
+    $dadosboleto["demonstrativo3"] = "Loja Virtual - http://loja.hot";
+    $dadosboleto["instrucoes1"] = "- Sr. Caixa, cobrar multa de 2% após o vencimento";
+    $dadosboleto["instrucoes2"] = "- Receber até 10 dias após o vencimento";
+    $dadosboleto["instrucoes3"] = "- Em caso de dúvidas entre em contato conosco: suporte@loja.hit.com.br";
+	$dadosboleto["instrucoes4"] = "&nbsp; Emitido pelo sistema Projeto BoletoPhp - www.boletophp.com.br";
+
+    // DADOS OPCIONAIS DE ACORDO COM O BANCO OU CLIENTE
+	$dadosboleto["quantidade"] = "001";
+	$dadosboleto["valor_unitario"] = $valor_boleto;
+	$dadosboleto["aceite"] = "";
 	$dadosboleto["especie"] = "R$";
-	$dadosboleto["especie_doc"] = "";
-	// ---------------------- DADOS FIXOS DE CONFIGURAÇÃO DO SEU BOLETO --------------- //
-	// DADOS DA SUA CONTA - ITAÚ
-	$dadosboleto["agencia"] = "1690"; // Num da agencia, sem digito
-	$dadosboleto["conta"] = "48781";	// Num da conta, sem digito
-	$dadosboleto["conta_dv"] = "2"; 	// Digito do Num da conta
-	// DADOS PERSONALIZADOS - ITAÚ
-	$dadosboleto["carteira"] = "175";  // Código da Carteira: pode ser 175, 174, 104, 109, 178, ou 157
-	// SEUS DADOS
-	$dadosboleto["identificacao"] = "Hcode Treinamentos";
-	$dadosboleto["cpf_cnpj"] = "24.700.731/0001-08";
-	$dadosboleto["endereco"] = "Rua Ademar Saraiva Leão, 234 - Alvarenga, 09853-120";
-	$dadosboleto["cidade_uf"] = "São Bernardo do Campo - SP";
-	$dadosboleto["cedente"] = "HCODE TREINAMENTOS LTDA - ME";
-	// NÃO ALTERAR!
+	$dadosboleto["especie_doc"] = "DS";
+// ---------------------- DADOS FIXOS DE CONFIGURAÇÃO DO SEU BOLETO --------------- //
+    // DADOS DA SUA CONTA - Bradesco
+    $dadosboleto["agencia"] = "2992"; // Num da agencia, sem digito
+    $dadosboleto["agencia_dv"] = "2"; // Digito do Num da agencia
+    $dadosboleto["conta"] = "0075253"; 	// Num da conta, sem digito
+    $dadosboleto["conta_dv"] = "3"; 	// Digito do Num da conta
+    // DADOS PERSONALIZADOS - Bradesco
+    $dadosboleto["conta_cedente"] = "0102003"; // ContaCedente do Cliente, sem digito (Somente Números)
+    $dadosboleto["conta_cedente_dv"] = "4"; // Digito da ContaCedente do Cliente
+    $dadosboleto["carteira"] = "06";  // Código da Carteira: pode ser 06 ou 03
+    // SEUS DADOS
+    $dadosboleto["identificacao"] = "Lojas Hit";
+    $dadosboleto["cpf_cnpj"] = "";
+    $dadosboleto["endereco"] = "Av. Santos Dumont, 243, Aflitos, 52050-050";
+    $dadosboleto["cidade_uf"] = "Recide - PE";
+    $dadosboleto["cedente"] = "HIT - ITA COMERCIOS Ltda.";
+    // NÃO ALTERAR!
 	$path = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "res" . DIRECTORY_SEPARATOR . "boletophp" . DIRECTORY_SEPARATOR . "include" . DIRECTORY_SEPARATOR;
-	require_once($path . "funcoes_itau.php");
-	require_once($path . "layout_itau.php");
+    require_once($path . "funcoes_bradesco.php");
+	require_once($pach . "layout_bradesco.php");
 });
 $app->get("/profile/orders", function(){
 	User::verifyLogin(false);
